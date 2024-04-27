1973e32ca9aa899faa0e059d1ff2cb37d7b9378b
siddharthab100 <125187435+siddharthab100@users.noreply.github.com>
2024-04-27 20:05:26 +0530

    Create EgDog.py

---
+++ EgDog.py
@@ -0,0 +1,8 @@
+class Dog:
+  def__init__(self,name,age):
+    self.name = name
+    self.age = age
+  def sit(self):
+    print(f"{self.name} is now sitting!")
+  def roll_over(self):
+    print(f"{self.name} has rolled over!")
