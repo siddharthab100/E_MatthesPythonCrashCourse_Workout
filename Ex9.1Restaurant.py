f70ac347f5d9e50a77c01dbe901052d0dadefb58
siddharthab100 <125187435+siddharthab100@users.noreply.github.com>
2024-04-27 20:11:57 +0530

    Create Ex9.1Restaurant.py

---
+++ Ex9.1Restaurant.py
@@ -0,0 +1,9 @@
+class Restaurant:
+  def __init__(self, restaurant_name, cuisine_type):
+    self.restaurant_name = restaurant_name
+    self.cuisine_type = cuisine_type
+  def describe_reataurant(self):
+    print(f"The {restaurant_name} does the {cuisine_type}.")
+  def o[en_restaurrant(self):
+    print(f"The {restaurant_name} is now open.")
+    
